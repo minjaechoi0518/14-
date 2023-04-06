5662a7c3f85a07e7af43be674894e7b3a88c1ed5
minjaechoi0518 <cmk7892@gmail.com>
2023-04-06 14:18:03 +0900

    3번째 시도 부분 수정완료

--- team_repo/CountBB.js
+++ team_repo/CountBB.js
@@ -1,40 +1,47 @@
-let random = []
+let crandom = []
+try_nums = 0
 
-while (random.length < 3) {
-    const bbRandom = Math.floor(Math.random() * 10)
-    if (!random.includes(bbRandom)) {
-        random.push(bbRandom)
+while (crandom.length < 3) {
+    let bbrandom = Math.floor(Math.random() * 10)
+    if (!crandom.includes(bbrandom)) {
+        crandom.push(bbrandom)
     }
 }
+
 function bb() {
-    const bb = document.getElementById('user-input').value
-    if (bb.length !== 3) {
-        alert('숫자 3개를 입력하세요')
+    let user = document.getElementById("user-input").value
+    if (user.length !== 3) {
+        alert("숫자 3개를 입력하세요")
     }
-}
 
-let b = 0
-let s = 0
+    let b = 0
+    let s = 0
 
-for (let i = 0; i < 3; i++) {
-    const random = random[i]
-    const bb = Number(bb[i])
 
-    if (random === bb) {
-        s++
-    } else if (random.includes(bb)) {
-        b++
+    for (let i = 0; i < 3; i++) {
+        let random = crandom[i]
+        let bb = Number(user[i])
+
+        if (random === bb) {
+            s++
+        } else if (crandom.includes(bb)) {
+            b++
+        }
+        
     }
-}
 
-let temp_html = `<li>
-                    <p>${실행횟수}시도</p>
-                    <p>${b}B${s}S</p>
-                </li>`
-$('#try-list').append(temp_html)
+    let temp_html = `<li>
+                        <p>${try_nums}번째 시도</p>
+                        <p>입력숫자${user}</p>
+                        <p>${b}B ${s}S</p>
+                    </li>`
+    $("#try-list").append(temp_html)
+    try_nums++
 
+    if (s === 3) {
+        alert('3 스트라이크 아웃! 게임을 종료합니다!')
+        window.location.reload()
+    } 
 
-if (s === 3) {
-    alert('3 스트라이크 아웃! 게임을 종료합니다!')
-}
-
+    };
+    
